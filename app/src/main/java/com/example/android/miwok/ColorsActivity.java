720436d69dd3d275ba31b972677820f302c947b0
ngogonggongngo.0000001@gmail.com <(:abcde12345:)>
2017-05-22 15:57:04 -0700

    ud839-lesson4-before-step24

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -77,6 +77,13 @@ public class ColorsActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -80,6 +80,14 @@ public class FamilyActivity extends AppCompatActivity {
         });
 
     }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */
