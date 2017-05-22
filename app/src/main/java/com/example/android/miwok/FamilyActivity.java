8cebfb28d786c539fd871abdddacdf95a5a8e9b1
ngogonggongngo.0000001@gmail.com <(:abcde12345:)>
2017-05-22 13:23:23 -0700

    ud839-lesson4-after-step17

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -68,9 +68,32 @@ public class FamilyActivity extends AppCompatActivity {
                 mMediaPlayer = MediaPlayer.create(view.getContext(), words.get(i).getmAudioResourceId());
                 mMediaPlayer.start();
                 Toast.makeText(FamilyActivity.this, "hello i=" + i + ", l=" +l, Toast.LENGTH_SHORT).show();
+                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        releaseMediaPlayer();
+                        Toast.makeText(FamilyActivity.this, "i am done", Toast.LENGTH_LONG).show();
+                    }
+                });
 
             }
         });
 
+    }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -70,9 +70,33 @@ public class NumbersActivity extends AppCompatActivity {
                 mMediaPlayer.start();
                 Toast.makeText(NumbersActivity.this, "hello i=" + i + ", l=" +l, Toast.LENGTH_SHORT).show();
 
+                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        releaseMediaPlayer();
+                        Toast.makeText(NumbersActivity.this, "i am done", Toast.LENGTH_LONG).show();
+                    }
+                });
+
             }
         });
 
 
+    }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
