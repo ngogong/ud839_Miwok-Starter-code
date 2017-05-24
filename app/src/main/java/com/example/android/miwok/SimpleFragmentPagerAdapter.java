ed22b4a16d73d89ab5411d849babffe957a50469
ngogonggongngo.0000001@gmail.com <(:abcde12345:)>
2017-05-24 16:33:44 -0700

    ud839-lesson5-after-step12-1

---
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by ismile on 5/24/2017.
+ */
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1){
+            return new FamilyFragment();
+        } else if (position == 2){
+            return new ColorsFragment();
+        } else if (position == 3){
+            return new PhrasesFragment();
+        }
+        return new NumbersFragment();
+
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
