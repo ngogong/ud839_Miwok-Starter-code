a1fef7d97d0ff8533ae0a7709dad6a3e72b1daba
ngogonggongngo.0000001@gmail.com <(:abcde12345:)>
2017-05-24 17:52:39 -0700

    ud839-lesson5-after-step14

--- app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -3,18 +3,24 @@ package com.example.android.miwok;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 /**
  * Created by ismile on 5/24/2017.
  */
 
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    private static final String LOG_TAG = SimpleFragmentPagerAdapter.class.getName();
+
+    private String tabTitles[] = new String[] { "Numbers", "Family", "Colors", "Phrases" };
+
     public SimpleFragmentPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
+        Log.e(LOG_TAG, " simplefragmentpageradpater getitem=" + position + " end");
         if (position == 0) {
             return new NumbersFragment();
         } else if (position == 1){
@@ -29,7 +35,15 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
     }
 
     @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        Log.e(LOG_TAG, " simplefragmentpageradpater getpagetitle=" + position + " end");
+        return tabTitles[position];
+    }
+
+    @Override
     public int getCount() {
+        Log.e(LOG_TAG, " simplefragmentpageradpater getcount end");
         return 4;
     }
 }
